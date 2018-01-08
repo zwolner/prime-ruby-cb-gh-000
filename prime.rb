35b467c296dfdd68ffc0bff5e1f4566999a4b5a5
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-08 19:52:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def prime?(num)
   if num == 0 || num.even? == true || num != num.abs
     return false
   else
-    range = (2...num.abs).to_a
+    range = (2...num).to_a
     range.delete_if {|x| (num % x) != 0}
   end
   range.empty? ? true : false
