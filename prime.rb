7307f95ffc18f2b1b219468e67649289d69f0ab5
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-08 19:25:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(num)
   if num == 0
-    return true
+    return false
   else
     range = Array.new(num.abs-1){2...num.abs}
     range.any? {|x| num % x == 0 ? false : true}
