00af874961d9f882c4e220a2bc58163aa9dc40ef
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-08 20:13:34 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+require 'benchmark'
+
+
+def prime?(num)
+  if num == 0 || num.even? == true || num != num.abs || num == 1
+    return false
+  else
+    range = (2...num).to_a
+    range.delete_if {|x| (num % x) != 0}
+  end
+  range.empty? ? true : false
+end
