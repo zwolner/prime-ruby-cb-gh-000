77bcc9aba913ce3b53963015e74d1b8d95992a5d
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-08 20:06:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,14 @@
 # Add  code here!
 require 'benchmark'
 
-def prime?(num)
-  if num == 0 || num.even? == true || num != num.abs || num == 1
-    return false
-  else
-    range = (2...num).to_a
-    range.delete_if {|x| (num % x) != 0}
+Benchmark.bmbm do |x|
+  def prime?(num)
+    if num == 0 || num.even? == true || num != num.abs || num == 1
+      return false
+    else
+      range = (2...num).to_a
+      range.delete_if {|x| (num % x) != 0}
+    end
+    range.empty? ? true : false
   end
-  range.empty? ? true : false
 end
-
-puts Benchmark.measure {"prime?(19)"*1_000_000}
