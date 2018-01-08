91b38c4e0fd5c9f4da3adb5db1a82bede0316bcc
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-08 19:52:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
 def prime?(num)
-  range = (2...num.abs).to_a
   if num == 0 || num.even? == true || num != num.abs
     return false
   else
+    range = (2...num.abs).to_a
     range.delete_if {|x| (num % x) != 0}
   end
   range.empty? ? true : false
